89085a0e0d8e27bab74fbb3fb2b60bf19afdc0b7
SistemasTopo <auxiliar.it@topo-int.com>
2022-02-16 11:54:54 -0600

    menu

--- resources/views/layouts/menuAdmin.blade.php
+++ resources/views/layouts/menuAdmin.blade.php
@@ -4,10 +4,27 @@
     </a>
     <ul class="sub-menu">
         <li>
-            <a href="/resumen" style="cursor: pointer">{{ __('Resumen General ') }}<i class="fas fa-paste"></i></a>
+            <a href="/resumen" style="cursor: pointer">{{ __('Resumen General ') }}<i class="fas fa-align-justify"></i></a>
         </li>
         <li>
-            <a href="/estadisticas" style="cursor: pointer">{{ __('Estadisticas ') }}<i class="fas fa-paste"></i></a>
+            <a href="/estadisticas" style="cursor: pointer">{{ __('Estadisticas ') }}<i class="fas fa-chart-bar"></i></a>
+        </li>
+    </ul>
+</li>
+
+<li style="font-size: 12px;" class="dropdown">
+    <a href="#" class="sub-menu-toggle">
+        {{ __('Registros ') }} <span class="caret"><i class="fas fa-clipboard-list"></i></span>
+    </a>
+    <ul class="sub-menu">
+        <li>
+            <a href="/servicio" style="cursor: pointer">{{ __('Servicio Transporte ') }}<i class="fas fa-list-alt"></i></a>
+        </li>
+        <li>
+            <a href="/checkList" style="cursor: pointer">{{ __('CheckList ') }}<i class="fas fa-clipboard-check"></i></a>
+        </li>
+        <li>
+            <a href="/horometro" style="cursor: pointer">{{ __('Horometros ') }}<i class="fas fa-business-time"></i></a>
         </li>
     </ul>
 </li>
@@ -18,30 +35,13 @@
     </a>
     <ul class="sub-menu">
         <li>
-            <a href="/servicio" style="cursor: pointer">{{ __('Servicio Transporte ') }}<i class="fas fa-paste"></i></a>
+            <a href="/servicio/create" style="cursor: pointer">{{ __('Servicio Transporte ') }}<i class="fas fa-list-alt"></i></a>
         </li>
         <li>
-            <a href="/checkList" style="cursor: pointer">{{ __('CheckList ') }}<i class="fas fa-paste"></i></a>
+            <a href="/checkList/create" style="cursor: pointer">{{ __('CheckList ') }}<i class="fas fa-clipboard-check"></i></a>
         </li>
         <li>
-            <a href="/horometro" style="cursor: pointer">{{ __('Horometros ') }}<i class="fas fa-paste"></i></a>
-        </li>
-    </ul>
-</li>
-
-<li style="font-size: 12px;" class="dropdown">
-    <a href="#" class="sub-menu-toggle">
-        {{ __('Registro ') }} <span class="caret"><i class="fas fa-indent"></i></span>
-    </a>
-    <ul class="sub-menu">
-        <li>
-            <a href="/servicio/create" style="cursor: pointer">{{ __('Servicio Transporte ') }}<i class="fas fa-paste"></i></a>
-        </li>
-        <li>
-            <a href="/checkList/create" style="cursor: pointer">{{ __('CheckList ') }}<i class="fas fa-paste"></i></a>
-        </li>
-        <li>
-            <a href="/horometro/create" style="cursor: pointer">{{ __('Horometros ') }}<i class="fas fa-paste"></i></a>
+            <a href="/horometro/create" style="cursor: pointer">{{ __('Horometros ') }}<i class="fas fa-business-time"></i></a>
         </li>
     </ul>
 </li>
--- resources/views/layouts/menuCliente.blade.php
+++ resources/views/layouts/menuCliente.blade.php
@@ -4,18 +4,18 @@
     </a>
     <ul class="sub-menu">
         <li>
-            <a href="/checkList/create" style="cursor: pointer">{{ __('Nuevo CheckList ') }}<i class="fa-solid fa-check-to-slot"></i></a>
+            <a href="/checkList/create" style="cursor: pointer">{{ __('Nuevo CheckList ') }}<i class="fas fa-calendar-check"></i></a>
         </li>
     </ul>
 </li>
 
 <li style="font-size: 12px;" class="dropdown">
     <a href="#" class="sub-menu-toggle">
-        {{ __('Horómetro ') }} <span class="caret"><i class="fa-solid fa-clock"></i></span>
+        {{ __('Horómetro ') }} <span class="caret"><i class="fas fa-business-time"></i></span>
     </a>
     <ul class="sub-menu">
         <li>
-            <a href="/horometro/create" style="cursor: pointer">{{ __('Nuevo Registro ') }}<i class="fa-solid fa-calendar-circle-plus"></i></a>
+            <a href="/horometro/create" style="cursor: pointer">{{ __('Nuevo Registro ') }}<i class="fas fa-stopwatch"></i></a>
         </li>
     </ul>
 </li>
@@ -26,7 +26,7 @@
     </a>
     <ul class="sub-menu">
         <li>
-            <a href="/servicio/create" style="cursor: pointer">{{ __('Nuevo Servicio ') }}<i class="fa-solid fa-clipboard-list"></i></a>
+            <a href="/servicio/create" style="cursor: pointer">{{ __('Nuevo Servicio ') }}<i class="fas fa-paste"></i></a>
         </li>
     </ul>
 </li>
