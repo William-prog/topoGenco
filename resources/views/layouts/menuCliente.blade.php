080ae271de2ed4f87a6f6d35fdf40446537f0870
SistemasTopo <auxiliar.it@topo-int.com>
2022-04-13 10:36:22 -0500

    Mejoras

--- resources/views/layouts/menuCliente.blade.php
+++ resources/views/layouts/menuCliente.blade.php
@@ -1,38 +1,16 @@
 <li style="font-size: 12px;" class="dropdown">
     <a href="#" class="sub-menu-toggle">
-        {{ __('CheckList ') }} <span class="caret"><i class="fas fa-clipboard-check"></i></span>
+        {{ __('Resumen Tecnico ') }} <span class="caret"><i class="fas fa-indent"></i></span>
     </a>
     <ul class="sub-menu">
         <li>
-            <a href="/checkList/create" style="cursor: pointer">{{ __('Nuevo CheckList ') }}<i class="fas fa-calendar-check"></i></a>
+            <a href="/resumen" style="cursor: pointer">{{ __('Resumen General ') }}<i class="fas fa-align-justify"></i></a>
         </li>
-    </ul>
-</li>
-
-<li style="font-size: 12px;" class="dropdown">
-    <a href="#" class="sub-menu-toggle">
-        {{ __('Horómetro ') }} <span class="caret"><i class="fas fa-business-time"></i></span>
-    </a>
-    <ul class="sub-menu">
         <li>
-            <a href="/horometro/create" style="cursor: pointer">{{ __('Nuevo Registro ') }}<i class="fas fa-stopwatch"></i></a>
+            <a href="/home" style="cursor: pointer">{{ __('Mantenimientos ') }}<i class="fas fa-chart-bar"></i></a>
         </li>
-    </ul>
-</li>
-
-<li style="font-size: 12px;" class="dropdown">
-    <a href="#" class="sub-menu-toggle">
-        {{ __('Servicio ') }} <span class="caret"><i class="fas fa-indent"></i></span>
-    </a>
-    <ul class="sub-menu">
         <li>
-            <a href="/servicio/create" style="cursor: pointer">{{ __('Nuevo Servicio ') }}<i class="fas fa-paste"></i></a>
+            <a href="/estadisticas" style="cursor: pointer">{{ __('Estadisticas ') }}<i class="fas fa-chart-bar"></i></a>
         </li>
     </ul>
-</li>
-
-<li style="font-size: 12px;" class="dropdown">
-    <a href="/home" class="sub-menu-toggle">
-        {{ __('Mantenimientos ') }} <span class="caret"><i class="fas fa-indent"></i></span>
-    </a>
 </li>
--- resources/views/resumenGeneral/estadisticas.blade.php
+++ resources/views/resumenGeneral/estadisticas.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
 <div class="container">
-
+    <div class="col-lg-12 mx-auto mb-5 text-black text-center">
+        <h1 class="display-4">Sin datos para realizar estadisticas</h1>
+    </div>
 </div>
 @include('layouts.footer')
 @endsection
