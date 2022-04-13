5f2916103ff1588d5ac013ac031a4f6078896b5a
SistemasTopo <auxiliar.it@topo-int.com>
2022-04-13 11:46:10 -0500

    Mejoras

--- resources/views/resumenGeneral/estadisticasInformacionGeneral.blade.php
+++ resources/views/resumenGeneral/estadisticasInformacionGeneral.blade.php
@@ -29,11 +29,7 @@
             var Fecha_Desde = $('#Fecha_Desde_General').val();
             var Fecha_Hasta = $('#Fecha_Hasta_General').val();
 
-            estadisticasGeneralServicio();
-
-            estadisticasGeneralHorometro();
-
-            estadisticasGeneralCartas();
+            
         }
     </script>
 
@@ -42,7 +38,7 @@
             <div class="col">
                 <input type="text" class="form-control" name="datoTOPOEntrada" id="datoTOPOEntrada">
             </div>
-            <div class="col">
+            <!-- <div class="col">
                 <input type="text" class="form-control" name="datoCOYMSAEntrada" id="datoCOYMSAEntrada">
             </div>
             <div class="col">
@@ -59,7 +55,7 @@
             </div>
             <div class="col">
                 <input type="text" class="form-control" name="datoCLMEntrada" id="datoCLMEntrada">
-            </div>
+            </div> -->
         </div>
 
         <div class="row m-3">
@@ -120,169 +116,7 @@
             var Fecha_Desde = $('#Fecha_Desde_General').val();
             var Fecha_Hasta = $('#Fecha_Hasta_General').val();
 
-            let cantEntradaTopo = jsonServicioEntradaTopo.filter(function(type, index) {
-                return type.fechaRegistroEntrada >= Fecha_Desde && type.fechaRegistroEntrada <= Fecha_Hasta
-            });
-
-            let cantEntradaFercons = jsonServicioEntradaFercons.filter(function(type, index) {
-                return type.fechaRegistroEntrada >= Fecha_Desde && type.fechaRegistroEntrada <= Fecha_Hasta
-            });
-
-            let cantEntradaFresnillo = jsonServicioEntradaFresnillo.filter(function(type, index) {
-                return type.fechaRegistroEntrada >= Fecha_Desde && type.fechaRegistroEntrada <= Fecha_Hasta
-            });
-
-            let cantEntradaCoymsa = jsonServicioEntradaCoymsa.filter(function(type, index) {
-                return type.fechaRegistroEntrada >= Fecha_Desde && type.fechaRegistroEntrada <= Fecha_Hasta
-            });
-
-            let cantEntradaClm = jsonServicioEntradaClm.filter(function(type, index) {
-                return type.fechaRegistroEntrada >= Fecha_Desde && type.fechaRegistroEntrada <= Fecha_Hasta
-            });
-
-            let cantEntradaTti = jsonServicioEntradaTti.filter(function(type, index) {
-                return type.fechaRegistroEntrada >= Fecha_Desde && type.fechaRegistroEntrada <= Fecha_Hasta
-            });
-
-            let cantEntradaOmytc = jsonServicioEntradaOmytc.filter(function(type, index) {
-                return type.fechaRegistroEntrada >= Fecha_Desde && type.fechaRegistroEntrada <= Fecha_Hasta
-            });
-
-            let cantEntradaOssa = jsonServicioEntradaOssa.filter(function(type, index) {
-                return type.fechaRegistroEntrada >= Fecha_Desde && type.fechaRegistroEntrada <= Fecha_Hasta
-            });
-
-
-
-
-
-
-            let cantSalidaTopo = jsonServicioSalidaTopo.filter(function(type, index) {
-                return type.fechaRegistroSalida >= Fecha_Desde && type.fechaRegistroSalida <= Fecha_Hasta
-            });
-
-            let cantSalidaFercons = jsonServicioSalidaFercons.filter(function(type, index) {
-                return type.fechaRegistroSalida >= Fecha_Desde && type.fechaRegistroSalida <= Fecha_Hasta
-            });
-
-            let cantSalidaFresnillo = jsonServicioSalidaFresnillo.filter(function(type, index) {
-                return type.fechaRegistroSalida >= Fecha_Desde && type.fechaRegistroSalida <= Fecha_Hasta
-            });
-
-            let cantSalidaCoymsa = jsonServicioSalidaCoymsa.filter(function(type, index) {
-                return type.fechaRegistroSalida >= Fecha_Desde && type.fechaRegistroSalida <= Fecha_Hasta
-            });
-
-            let cantSalidaClm = jsonServicioSalidaClm.filter(function(type, index) {
-                return type.fechaRegistroSalida >= Fecha_Desde && type.fechaRegistroSalida <= Fecha_Hasta
-            });
-
-            let cantSalidaTti = jsonServicioSalidaTti.filter(function(type, index) {
-                return type.fechaRegistroSalida >= Fecha_Desde && type.fechaRegistroSalida <= Fecha_Hasta
-            });
-
-            let cantSalidaOmytc = jsonServicioSalidaOmytc.filter(function(type, index) {
-                return type.fechaRegistroSalida >= Fecha_Desde && type.fechaRegistroSalida <= Fecha_Hasta
-            });
-
-            let cantSalidaOssa = jsonServicioSalidaOssa.filter(function(type, index) {
-                return type.fechaRegistroSalida >= Fecha_Desde && type.fechaRegistroSalida <= Fecha_Hasta
-            });
-
-            var total_Salida_Topo = cantSalidaTopo.reduce((sum, value) => (typeof parseInt(value.cantidadSalida) == "number" ? sum + parseInt(value.cantidadSalida) : sum), 0);
-            var total_Salida_Fercons = cantSalidaFercons.reduce((sum, value) => (typeof parseInt(value.cantidadSalida) == "number" ? sum + parseInt(value.cantidadSalida) : sum), 0);
-            var total_Salida_Fresnillo = cantSalidaFresnillo.reduce((sum, value) => (typeof parseInt(value.cantidadSalida) == "number" ? sum + parseInt(value.cantidadSalida) : sum), 0);
-            var total_Salida_Coymsa = cantSalidaCoymsa.reduce((sum, value) => (typeof parseInt(value.cantidadSalida) == "number" ? sum + parseInt(value.cantidadSalida) : sum), 0);
-            var total_Salida_Clm = cantSalidaClm.reduce((sum, value) => (typeof parseInt(value.cantidadSalida) == "number" ? sum + parseInt(value.cantidadSalida) : sum), 0);
-            var total_Salida_Tti = cantSalidaTti.reduce((sum, value) => (typeof parseInt(value.cantidadSalida) == "number" ? sum + parseInt(value.cantidadSalida) : sum), 0);
-            var total_Salida_Omytc = cantSalidaOmytc.reduce((sum, value) => (typeof parseInt(value.cantidadSalida) == "number" ? sum + parseInt(value.cantidadSalida) : sum), 0);
-            var total_Salida_Ossa = cantSalidaOssa.reduce((sum, value) => (typeof parseInt(value.cantidadSalida) == "number" ? sum + parseInt(value.cantidadSalida) : sum), 0);
-
-
-            var total_Entrada_Topo = cantEntradaTopo.reduce((sum, value) => (typeof parseInt(value.cantidadEntrada) == "number" ? sum + parseInt(value.cantidadEntrada) : sum), 0);
-            var total_Entrada_Fercons = cantEntradaFercons.reduce((sum, value) => (typeof parseInt(value.cantidadEntrada) == "number" ? sum + parseInt(value.cantidadEntrada) : sum), 0);
-            var total_Entrada_Fresnillo = cantEntradaFresnillo.reduce((sum, value) => (typeof parseInt(value.cantidadEntrada) == "number" ? sum + parseInt(value.cantidadEntrada) : sum), 0);
-            var total_Entrada_Coymsa = cantEntradaCoymsa.reduce((sum, value) => (typeof parseInt(value.cantidadEntrada) == "number" ? sum + parseInt(value.cantidadEntrada) : sum), 0);
-            var total_Entrada_Clm = cantEntradaClm.reduce((sum, value) => (typeof parseInt(value.cantidadEntrada) == "number" ? sum + parseInt(value.cantidadEntrada) : sum), 0);
-            var total_Entrada_Tti = cantEntradaTti.reduce((sum, value) => (typeof parseInt(value.cantidadEntrada) == "number" ? sum + parseInt(value.cantidadEntrada) : sum), 0);
-            var total_Entrada_Omytc = cantEntradaOmytc.reduce((sum, value) => (typeof parseInt(value.cantidadEntrada) == "number" ? sum + parseInt(value.cantidadEntrada) : sum), 0);
-            var total_Entrada_Ossa = cantEntradaOssa.reduce((sum, value) => (typeof parseInt(value.cantidadEntrada) == "number" ? sum + parseInt(value.cantidadEntrada) : sum), 0);
-
-            var total_Topo = total_Salida_Topo + total_Entrada_Topo;
-            var total_Fercons = total_Salida_Fercons + total_Entrada_Fercons;
-            var total_Fresnillo = total_Salida_Fresnillo + total_Entrada_Fresnillo;
-            var total_Coymsa = total_Salida_Coymsa + total_Entrada_Coymsa;
-            var total_Clm = total_Salida_Clm + total_Entrada_Clm;
-            var total_Tti = total_Salida_Tti + total_Entrada_Tti;
-            var total_Omytc = total_Salida_Omytc + total_Entrada_Omytc;
-            var total_Ossa = total_Salida_Ossa + total_Entrada_Ossa;
-
-
-
-            var total = total_Topo + total_Fercons + total_Fresnillo + total_Coymsa + total_Clm + total_Tti + total_Omytc + total_Ossa;
-            var totalEntrada = total_Entrada_Topo + total_Entrada_Fercons + total_Entrada_Fresnillo + total_Entrada_Coymsa + total_Entrada_Clm + total_Entrada_Tti + total_Entrada_Omytc + total_Entrada_Ossa;
-            var totalSalida = total_Salida_Topo + total_Salida_Fercons + total_Salida_Fresnillo + total_Salida_Coymsa + total_Salida_Clm + total_Salida_Tti + total_Salida_Omytc + total_Salida_Ossa;
-
-            $("#totalEntrada").html(totalEntrada);
-
-            $("#totalSalida").html(totalSalida);
-
-            $("#totalTotales").html(total);
-
-
-            $("#totalServicio").html(total);
-
-            document.getElementById("datoTOPOTotal").value = total_Topo;
-            document.getElementById("datoCOYMSATotal").value = total_Coymsa;
-            document.getElementById("datoOMyTCTotal").value = total_Omytc;
-            document.getElementById("datoPLCTotal").value = total_Fresnillo;
-            document.getElementById("datoFERCONSTotal").value = total_Fercons;
-            document.getElementById("datoOSSATotal").value = total_Ossa;
-            document.getElementById("datoCLMTotal").value = total_Clm;
-
-            document.getElementById("datoTOPOEntrada").value = total_Entrada_Topo;
-            document.getElementById("datoCOYMSAEntrada").value = total_Entrada_Coymsa;
-            document.getElementById("datoOMyTCEntrada").value = total_Entrada_Omytc;
-            document.getElementById("datoPLCEntrada").value = total_Entrada_Fresnillo;
-            document.getElementById("datoFERCONSEntrada").value = total_Entrada_Fercons;
-            document.getElementById("datoOSSAEntrada").value = total_Entrada_Ossa;
-            document.getElementById("datoCLMEntrada").value = total_Entrada_Clm;
-
-            document.getElementById("datoTOPOSalida").value = total_Salida_Topo;
-            document.getElementById("datoCOYMSASalida").value = total_Salida_Coymsa;
-            document.getElementById("datoOMyTCSalida").value = total_Salida_Omytc;
-            document.getElementById("datoPLCSalida").value = total_Salida_Fresnillo;
-            document.getElementById("datoFERCONSSalida").value = total_Salida_Fercons;
-            document.getElementById("datoOSSASalida").value = total_Salida_Ossa;
-            document.getElementById("datoCLMSalida").value = total_Salida_Clm;
-
-            $("#totalTOPO").html(total_Topo);
-            $("#totalCOYMSA").html(total_Coymsa);
-            $("#totalOMyTC").html(total_Omytc);
-            $("#totalPLC").html(total_Fresnillo);
-            $("#totalFERCONS").html(total_Fercons);
-            $("#totalOSSA").html(total_Ossa);
-            $("#totalCLM").html(total_Clm);
-
-            $("#totalTOPOEntrada").html(total_Entrada_Topo);
-            $("#totalCOYMSAEntrada").html(total_Entrada_Coymsa);
-            $("#totalOMyTCEntrada").html(total_Entrada_Omytc);
-            $("#totalPLCEntrada").html(total_Entrada_Fresnillo);
-            $("#totalFERCONSEntrada").html(total_Entrada_Fercons);
-            $("#totalOSSAEntrada").html(total_Entrada_Ossa);
-            $("#totalCLMEntrada").html(total_Entrada_Clm);
-
-            $("#totalTOPOSalida").html(total_Salida_Topo);
-            $("#totalCOYMSASalida").html(total_Salida_Coymsa);
-            $("#totalOMyTCSalida").html(total_Salida_Omytc);
-            $("#totalPLCSalida").html(total_Salida_Fresnillo);
-            $("#totalFERCONSSalida").html(total_Salida_Fercons);
-            $("#totalOSSASalida").html(total_Salida_Ossa);
-            $("#totalCLMSalida").html(total_Salida_Clm);
-
-            $("#totalMayorTOPO").html(total_Topo);
-            $("#totalMayorFresnillo").html(total_Fresnillo);
-            $("#totalMayorOMyTC").html(total_Omytc);
+            
 
         }
     </script>
@@ -291,7 +125,7 @@
         <div class="col">
             <div class="card" style="width: 100%;">
                 <div class="card-body">
-                    <h5 class="card-title">Servicio Elevador Alimak</h5>
+                    <h5 class="card-title">Servicio Genco</h5>
                     <h6 class="card-subtitle mb-2 text-muted">Resumen</h6>
                     <p class="card-text">El número de servicios no contempla entrada/salida sino el traslado de personal a diferentes niveles, el registro puede obtener de los reportes diarios.</p>
                     <p class="card-text">En el período seleccionado del presente año se presentaron <strong id="totalServicio"> </strong> servicios de entrada y salida.</p>
@@ -403,32 +237,6 @@
 
 
     <style>
-        .pricing-table-title {
-            text-transform: uppercase;
-            font-weight: 700;
-            font-size: 2.6em;
-            color: #FFF;
-            margin-top: 15px;
-            text-align: left;
-            margin-bottom: 25px;
-            text-shadow: 0 1px 1px rgba(0, 0, 0, 0.4);
-        }
-
-        .pricing-table-title a {
-            font-size: 0.6em;
-        }
-
-        .clearfix:after {
-            content: '';
-            display: block;
-            height: 0;
-            width: 0;
-            clear: both;
-        }
-
-        /** ========================
- * Contenedor
- ============================*/
         .pricing-wrapper {
             width: 960px;
             margin: 40px auto 0;
@@ -577,9 +385,7 @@
             background: #228799;
         }
 
-        /** ================
- * Responsive
- ===================*/
+        
         @media only screen and (min-width: 768px) and (max-width: 959px) {
             .pricing-wrapper {
                 width: 768px;
@@ -622,17 +428,11 @@
         <div class="pricing-table entrada">
             <h3 class="pricing-title">Entradas</h3>
             <div class="price">
-                <h3> <sup id="totalEntrada"> </sup> <sup>/ servicios</sup> </h3>
+                 <sup id="totalEntrada"> </sup> <sup>/ servicios</sup>
             </div>
             <!-- Lista de Caracteristicas / Propiedades -->
             <ul class="table-list">
                 <li> <span>TOPO</span> <span id="totalTOPOEntrada" class="badge bg-secondary"></span> </li>
-                <li> <span>Fresnillo PLC</span> <span id="totalPLCEntrada" class="badge bg-secondary"></span> </li>
-                <li> <span>OMyTC</span> <span id="totalOMyTCEntrada" class="badge bg-secondary"></span> </li>
-                <li> <span>FERCONS</span> <span id="totalFERCONSEntrada" class="badge bg-secondary"></span> </li>
-                <li> <span>COYMSA</span> <span id="totalCOYMSAEntrada" class="badge bg-secondary"></span> </li>
-                <li> <span>CLM</span> <span id="totalCLMEntrada" class="badge bg-secondary"></span> </li>
-                <li> <span>OSSA</span> <span id="totalOSSAEntrada" class="badge bg-secondary"></span> </li>
                 <!-- <li>Cuentas de correo <span class="unlimited">ilimitadas</span></li> -->
             </ul>
         </div>
@@ -643,12 +443,6 @@
             <!-- Lista de Caracteristicas / Propiedades -->
             <ul class="table-list">
                 <li> <span>TOPO</span> <span id="totalTOPOSalida" class="badge bg-secondary"></span> </li>
-                <li> <span>Fresnillo PLC</span> <span id="totalPLCSalida" class="badge bg-secondary"></span> </li>
-                <li> <span>OMyTC</span> <span id="totalOMyTCSalida" class="badge bg-secondary"></span> </li>
-                <li> <span>FERCONS</span> <span id="totalFERCONSSalida" class="badge bg-secondary"></span> </li>
-                <li> <span>COYMSA</span> <span id="totalCOYMSASalida" class="badge bg-secondary"></span> </li>
-                <li> <span>CLM</span> <span id="totalCLMSalida" class="badge bg-secondary"></span> </li>
-                <li> <span>OSSA</span> <span id="totalOSSASalida" class="badge bg-secondary"></span> </li>
             </ul>
         </div>
 
@@ -658,12 +452,6 @@
             <!-- Lista de Caracteristicas / Propiedades -->
             <ul class="table-list">
                 <li> <span>TOPO</span> <span id="totalTOPO" class="badge bg-secondary"></span> </li>
-                <li> <span>Fresnillo PLC</span> <span id="totalPLC" class="badge bg-secondary"></span> </li>
-                <li> <span>OMyTC</span> <span id="totalOMyTC" class="badge bg-secondary"></span> </li>
-                <li> <span>FERCONS</span> <span id="totalFERCONS" class="badge bg-secondary"></span> </li>
-                <li> <span>COYMSA</span> <span id="totalCOYMSA" class="badge bg-secondary"></span> </li>
-                <li> <span>CLM</span> <span id="totalCLM" class="badge bg-secondary"></span> </li>
-                <li> <span>OSSA</span> <span id="totalOSSA" class="badge bg-secondary"></span> </li>
             </ul>
         </div>
 
@@ -698,7 +486,7 @@
                 <h5 class="card-title" style="text-align: center;">Cartas de Mantenimiento</h5>
                 <div class="row m-3">
                     <div class="col-6">
-                        <h6 class="card-subtitle mb-2 text-muted">Cartas de mantenimiento 40Hr</h6>
+                        <h6 class="card-subtitle mb-2 text-muted">Cartas de mantenimiento 100Hr</h6>
                         <table id="cuarenta" class="table table-sm table-hover" style="background-color: white; border-collapse: collapse; border-radius: 1em; overflow: hidden;" cellspacing="0">
                             <thead class="">
                                 <tr>
@@ -730,7 +518,7 @@
 
                     </div>
                     <div class="col-6">
-                        <h6 class="card-subtitle mb-2 text-muted">Cartas de mantenimiento 120Hr</h6>
+                        <h6 class="card-subtitle mb-2 text-muted">Cartas de mantenimiento 50Hr</h6>
                         <table id="cientoveinte" class="table table-sm table-hover" style="background-color: white; border-collapse: collapse; border-radius: 1em; overflow: hidden;" cellspacing="0">
                             <thead class="">
                                 <tr>
@@ -741,7 +529,7 @@
                             </thead>
                             <p style="display:none;"> {{$contador=0}} </p>
                             <tbody>
-                                @foreach($registroMantenimieen as $datoCV)
+                                @foreach($registroMantenimientoCincuenta as $datoCV)
                                 <tr>
                                     <td>{{$contador = $contador + 1}}</td>
                                     <td>
